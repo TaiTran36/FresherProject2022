53132a6cc02eb3348481d19b623a8facf5e6c73c
Giang0606 <dieuly0606@gmail.com>
2022-03-16 00:49:03 +0700

    save inputed value

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -69,6 +69,6 @@ class LoginController extends Controller
  
         return back()->withErrors([
             'email' => 'The provided credentials do not match our records.',
-        ]);
+        ])->withInput($request->all());
     }
 }
