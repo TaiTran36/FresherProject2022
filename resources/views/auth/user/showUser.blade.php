aa8f3a7058cd2f1f5c37ba3e9c5afdd3c3d63f0a
Giang0606 <dieuly0606@gmail.com>
2022-03-26 00:06:39 +0700

    format code

--- resources/views/auth/user/showUser.blade.php
+++ resources/views/auth/user/showUser.blade.php
@@ -3,21 +3,22 @@
 @section('content-admin')
 <div class="container">
     <div class="row justify-content-center">
-                    <form method="GET" action="{{ route('user.show', $user->id) }}">
-                        @foreach ($fields as $f => $f_value) 
-                        <div class="row mb-3 mt-4">
-                            <label for={{$f}} class="col-md-3 col-form-label text-md-end">{{ __($f_value) }}</label>
+        <form method="GET" action="{{ route('user.show', $user->id) }}">
+            @foreach ($fields as $f => $f_value)
+            <div class="row mb-3 mt-4">
+                <label for={{$f}} class="col-md-3 col-form-label text-md-end">{{ __($f_value) }}</label>
 
-                            <div class="col-md-8">
-                                @if ($f == "photo_url")
-                                    <img id="preview-image" class="img img-fluid" style="max-width: 110px; height: auto;" src="{{asset('images/'.$user->$f)}}" alt="">
-                                @else 
-                                    <input id={{$f}} type="text" class="form-control" name={{$f}} value="{{$user->$f}}" disabled>
-                                @endif 
-                            </div>
-                        </div>
-                        @endforeach
-                    </form>
+                <div class="col-md-8">
+                    @if ($f == "photo_url")
+                    <img id="preview-image" class="img img-fluid" style="max-width: 110px; height: auto;"
+                        src="{{asset('images/'.$user->$f)}}" alt="">
+                    @else
+                    <input id={{$f}} type="text" class="form-control" name={{$f}} value="{{$user->$f}}" disabled>
+                    @endif
+                </div>
+            </div>
+            @endforeach
+        </form>
     </div>
 </div>
 @endsection
