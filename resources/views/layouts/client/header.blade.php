9894f62edea97ddf1319b1a585f72da0c73c8441
Giang0606 <dieuly0606@gmail.com>
2022-04-04 18:03:24 +0700

    add info user

--- resources/views/layouts/client/header.blade.php
+++ resources/views/layouts/client/header.blade.php
@@ -8,19 +8,23 @@
         </div>
 
         <div class="col-md-6 text-center order-1 order-md-2 mb-3 mb-md-0">
-            <a href="#" class="logo m-0 text-uppercase">
+            <a href="/" class="logo m-0 text-uppercase">
                 {{ config('app.name', 'Laravel') }}
             </a>
         </div>
 
-        {{-- <div class="col-md-3 text-end order-2 order-md-3 mb-3 mb-md-0">
+        <div class="col-md-3 text-end order-2 order-md-3 mb-3 mb-md-0">
             @auth
-                <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+            <div class="d-flex">
+                <img src="{{ asset('images/' . Auth::user()->photo_url) }}" class="image rounded-circle avatar mr-2"
+                    alt="Image">
+                <strong class="mt-3">{{ Auth::user()->username_login }}</strong>
+            </div>
             @else
-                <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">
-                    Log in
-                </a>
+            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">
+                {{ __('Log in') }}
+            </a>
             @endauth
-        </div> --}}
+        </div>
     </div>
 </nav>
