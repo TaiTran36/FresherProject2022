a96f4bf3c0f497c51c101db1fab4a67c56671b2b
Giang0606 <dieuly0606@gmail.com>
2022-04-06 22:27:30 +0700

    change navbar

--- resources/views/layouts/client/header.blade.php
+++ resources/views/layouts/client/header.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-md navbar-light bg-white">
+<nav class="navbar navbar-expand-md navbar-light bg-white p-2">
     <div class="container">
         <div class="col-md-3 order-3 order-md-1">
             <form action="#" class="search-form">
@@ -7,24 +7,56 @@
             </form>
         </div>
 
-        <div class="col-md-6 text-center order-1 order-md-2 mb-3 mb-md-0">
+        <div class="col-md-6 text-center order-1 order-md-2 mb-md-0">
             <a href="/" class="logo m-0 text-uppercase">
                 {{ config('app.name', 'Laravel') }}
             </a>
         </div>
 
-        <div class="col-md-3 text-end order-2 order-md-3 mb-3 mb-md-0">
-            @auth
-            <div class="d-flex">
-                <img src="{{ asset('images/' . Auth::user()->photo_url) }}" class="image rounded-circle avatar mr-2"
-                    alt="Image">
-                <strong class="mt-3">{{ Auth::user()->username_login }}</strong>
-            </div>
-            @else
-            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">
-                {{ __('Log in') }}
-            </a>
-            @endauth
+        <div class="col-md-3 offset-md-1 text-end order-2 order-md-3 mb-md-0 pl-5">
+            <ul class="navbar-nav ms-auto">
+                @guest
+                    @if (Route::has('login'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                    @endif
+
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                    @endif
+                @else
+                    <div class="d-flex">
+                        <img src="{{ asset('images/' . Auth::user()->photo_url) }}" class="image rounded-circle avatar mt-0" alt="">
+
+                        <li class="nav-item dropdown text-center">                        
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle mt-1" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->username_login }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-end mt-2" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('profile') }}">{{ __('Profile') }}</a>
+                                
+                                @if (Auth::check() && Auth::user()->role == 3)
+                                    <a class="dropdown-item" href="{{ route('post.create') }}">{{ __('Add post') }}</a>
+                                @endif
+
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    </div>
+                @endguest
+            </ul>
         </div>
     </div>
 </nav>
