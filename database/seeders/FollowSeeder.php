25a23d3ec04e7853b43a3932c36f20c5f48e7d56
Giang0606 <dieuly0606@gmail.com>
2022-04-21 15:06:44 +0700

    fix error seeder get id 0

--- database/seeders/FollowSeeder.php
+++ database/seeders/FollowSeeder.php
@@ -19,11 +19,12 @@ class FollowSeeder extends Seeder
         $countUser = User::count();
         
         for ($i = 0; $i < 50; $i++) {
+            $users_id = range(1, $countUser);
             $ed_er = array_rand(range(1, $countUser), 2);
 
             Follow::create([
-                'followed_id' => $ed_er[0], 
-                'follower_id' => $ed_er[1],
+                'followed_id' => $users_id[$ed_er[0]], 
+                'follower_id' => $users_id[$ed_er[1]],
             ]);
         }
     }
