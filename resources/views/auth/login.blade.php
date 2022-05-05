e5c0d2e43bd5d47b32bfe1076c8c168087bffb03
Giang0606 <dieuly0606@gmail.com>
2022-05-05 10:31:51 +0700

    change view login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -20,78 +21,103 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/login.css') }}" rel="stylesheet">
 </head>
+
 <body>
     <div id="login">
         <main>
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-5">
-                    <div class="card">
-                        <div class="card-body">
-                            <form method="POST" action="{{ route('login') }}">
-                                @csrf
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-5">
+                        <div class="card">
+                            <div class="card-body">
+                                <form method="POST" action="{{ route('login') }}">
+                                    @csrf
 
-                                <div class="row mb-2">
-                                    <b class="title text-center text-decoration-underline">
-                                        {{ __('ADMIN LOGIN') }}
-                                    </b>
-                                </div>
+                                    <div class="row mb-2">
+                                        <b class="title text-center text-decoration-underline">
+                                            {{ __('LOGIN') }}
+                                        </b>
+                                    </div>
 
-                                <div class="row mb-2">
-                                    <div class="col-md-10 offset-md-1">
-                                        <input 
-                                            id="email" 
-                                            type="text" 
-                                            class="form-control border-0 @error('email') is-invalid @enderror" 
-                                            name="email" 
-                                            value="{{ old('email') }}"
-                                            placeholder="Email address"  
-                                            autocomplete="email" autofocus>
+                                    <div class="row mb-2">
+                                        <div class="col-md-10 offset-md-1">
+                                            <input id="email" type="text"
+                                                class="form-control border-0 bg-light @error('email') is-invalid @enderror"
+                                                name="email" value="{{ old('email') }}" placeholder="Email address"
+                                                autocomplete="email" autofocus>
 
-                                        @error('email')
+                                            @error('email')
                                             <span class="invalid-feedback" role="alert">
                                                 <strong>{{ $message }}</strong>
                                             </span>
-                                        @enderror
+                                            @enderror
+                                        </div>
                                     </div>
-                                </div>
 
-                                <div class="row mb-2">
-                                    <div class="col-md-10 offset-md-1">
-                                        <input 
-                                            id="password" 
-                                            type="password" 
-                                            class="form-control border-0 @error('password') is-invalid @enderror" 
-                                            name="password" 
-                                            placeholder="Password"
-                                            autocomplete="current-password">
+                                    <div class="row mb-2">
+                                        <div class="col-md-10 offset-md-1">
+                                            <input id="password" type="password"
+                                                class="form-control border-0 bg-light @error('password') is-invalid @enderror"
+                                                name="password" placeholder="Password" autocomplete="current-password">
 
-                                        @error('password')
+                                            @error('password')
                                             <span class="invalid-feedback" role="alert">
                                                 <strong>{{ $message }}</strong>
                                             </span>
-                                        @enderror
+                                            @enderror
+                                        </div>
                                     </div>
-                                </div>
 
-                                <div class="row mb-3">
-                                    <div class="offset-md-1">
-                                        <button type="submit" class="col-md-10 btn bg-primary">
-                                            {{ __('SIGN IN') }}
-                                        </button>
+                                    <div class="row mb-1">
+                                        <div class="col-md-5 offset-md-1">
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" name="remember"
+                                                    id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                                <label class="form-check-label" for="remember">
+                                                    {{ __('Remember Me') }}
+                                                </label>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-5 offset-md-1 ml-0 p-0 text-right">
+                                            @if (Route::has('password.request'))
+                                                <a class="btn btn-link pl-0 py-0" style="font-size:15px;"
+                                                    href="{{ route('password.request') }}">
+                                                    {{ __('Forgot Your Password?') }}
+                                                </a>
+                                            @endif
+                                        </div>
                                     </div>
-                                </div>
-                            </form>
-                        </div>
 
-                        <div class="card-header">
-                            <div class="row mb-3"></div>
+                                    <div class="row mb-0">
+                                        <div class="offset-md-1">
+                                            <button type="submit" class="col-md-10 btn bg-primary">
+                                                {{ __('SIGN IN') }}
+                                            </button>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="offset-md-1">
+                                            <a class="btn btn-link pl-0 py-0" style="font-size:15px;"
+                                                href="{{ route('register') }}">
+                                                {{ __('Create a new account') }}
+                                            </a>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+
+                            <div class="card-header">
+                                <div class="row mb-3"></div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
         </main>
     </div>
 </body>
+
 </html>
